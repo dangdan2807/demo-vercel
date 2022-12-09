d593e0f311ae6bc568f907b51ba2a23b34ba8239
KayJuno <dangdan2807@gmail.com>
2022-12-09 23:17:58 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-
+const routes = require('./api/routes/index');
 app.use(express.json({ extended: false }));
 
-const product = require('./api/product');
-
-app.use('/api/product', product);
 const PORT = process.env.PORT || 8080;
+routes(app);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
